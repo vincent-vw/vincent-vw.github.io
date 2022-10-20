a058a774fe49fcf56f06c212fa24c92594e245b7
Vincent W <93243454+vincent-vw@users.noreply.github.com>
2022-10-20 16:26:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// window.addEventListener("scroll", function(event){
+//     var scroll = this.scrollY;
+//     if(scroll > 20){
+//         document.getElementById("nav_links").style.backgroundColor = "rgba(255, 255, 255, 0.300)";
+//         document.getElementById("nav_links").style.border = "0";
+//     } else {
+//         document.getElementById("nav_links").style.backgroundColor = "transparent";
+//         document.getElementById("nav_links").style.borderBottom = "0.5px solid";
+//     }
+// });
